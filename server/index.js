b8024df09f46f7d35ebe22ce23a4eaa8440921cb
abdullah628 <abdullah628360@gmail.com>
2024-03-15 16:59:59 +0600

    cors solved

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors(
     {
-        origin: ["https://mongo-crud-frontend.vercel.app/"],
+        origin: ["https://mongo-crud-frontend.vercel.app"],
         methods: ["POST", "GET", "PUT", "DELETE"],
         credentials: true
     }
