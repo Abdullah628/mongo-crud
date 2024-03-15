0e83e7009a8d7c86d4092510ebd46ff9c21c9a02
abdullah628 <abdullah628360@gmail.com>
2024-03-15 15:59:30 +0600

    project done

---
+++ client/src/components/getUser/User.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import toast from 'react-hot-toast';
+import axios from "axios";
+import "./user.css";
+
+const User = () => {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get("http://localhost:3000/api/getAll");
+      setUser(response.data);
+    };
+
+    fetchData();
+
+  }, []);
+
+  const deleteUser = async (userId) => {
+    await axios.delete(`http://localhost:3000/api/delete/${userId}`)
+    .then((res)=>{
+      setUser((prevUser)=> prevUser.filter((user)=> user._id !== userId))
+      toast.success(res.data.msg, {position: 'top-right'})
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
+  return (
+    <div className="userTable">
+      <Link to={"/add"} className="userAddBtn">
+        Add User
+      </Link>
+      <table border={1} cellSpacing={0}>
+        <thead>
+          <tr>
+            <th>S.No.</th>
+            <th>User Name</th>
+            <th>User Email</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            user.map((user,index)=>{
+              return(
+                <tr key = {index}>
+                <td>{index+1}</td>
+                <td>{user.fname} {user.lname}</td>
+                <td>{user.email}</td>
+                <td className="actionButtons">
+                  <button onClick={()=> deleteUser(user._id)}>Delete</button>
+                  <Link to={`/edit/`+ user._id} className="editBtn">
+                    Edit
+                  </Link>
+                </td>
+              </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default User;
---
+++ client/src/components/updateUser/Edit.jsx
@@ -0,0 +1,95 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+import { Link, useParams, useNavigate } from "react-router-dom";
+
+const Edit = () => {
+
+  const {id} = useParams();
+  const users = {
+    fname: "",
+    lname: "",
+    email: "",
+    
+  };
+  const [user, setUser] = useState(users);
+  const navigate = useNavigate();
+  const inputHandler = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  useEffect(()=>{
+    axios.get(`http://localhost:3000/api/getone/${id}`)
+    .then((res)=>{
+      
+      setUser(res.data)
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+  }, [id])
+  const submitForm = async (e) => {
+    e.preventDefault();
+    await axios
+      .put(`http://localhost:3000/api/update/${id}`, user)
+      
+      .then((res) => {
+        toast.success(res.data.msg, { position: "top-right" });
+        navigate("/");
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div className="addUser">
+      <Link to={"/"}>Back</Link>
+      <h3>Update user</h3>
+      <form className="addUserForm" onSubmit={submitForm}>
+        <div className="inputGroup">
+          <label htmlFor="fname">First name</label>
+          <input
+            type="text"
+            onChange={inputHandler}
+            id="fname"
+            name="fname"
+            value={user.fname}
+            autoComplete="off"
+            placeholder="First name"
+          />
+        </div>
+        <div className="inputGroup">
+          <label htmlFor="lname">Last name</label>
+          <input
+            type="text"
+            onChange={inputHandler}
+            id="lname"
+            name="lname"
+            value={user.lname}
+            autoComplete="off"
+            placeholder="Last name"
+          />
+        </div>
+        <div className="inputGroup">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            onChange={inputHandler}
+            id="email"
+            name="email"
+            value={user.email}
+            autoComplete="off"
+            placeholder="Email"
+          />
+        </div>
+
+        <div className="inputGroup">
+          <button type="submit">UPDATE USER</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Edit;
